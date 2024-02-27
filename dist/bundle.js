0e5395cb95649dedbcd800442951c0fa44ce938f
Trevor22 <kasasatrevor25@gmail.com>
2024-02-27 14:40:42 +0300

    created webpack To do list project

---
+++ dist/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("console.log(\"testing webpack\")\r\n\r\nconst contentDiv = document.querySelector(\".content\")\r\nconst test = document.createElement(\"h1\")\r\ntest.textContent = \"Hello, this is Trevor\"\r\ncontentDiv.appendChild(test)\n\n//# sourceURL=webpack://todo/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+console.log("testing webpack")
+
+const contentDiv = document.querySelector(".content")
+const test = document.createElement("h1")
+test.textContent = "Hello, this is Trevor"
+contentDiv.appendChild(test)
